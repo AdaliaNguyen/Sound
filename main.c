d3a269213f885daeebdbae7cdd234e01fcd4a013
AdaliaNguyen <thao.nguyen.6.2.1991@gmail.com>
2018-04-03 08:31:29 +0000

    first commit

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sound.h"
+#include <signal.h>
 
 int main(void) {
+	int ret;
 	while(1){
 		// record 1 sec of sound into test1.wav
-		system("arecord -r16000 -c1 -f S16_LE -d1 test1.wav");
+		ret = system("arecord -r16000 -q -c1 -f S16_LE -d1 test1.wav");
+		if(WIFSIGNALED(ret) && (WTERMSIG(ret)==SIGINT)) break;
 		// open the wav file and read samples
 		displayWAVheader("test1.wav");
 		
@@ -14,6 +17,6 @@ int main(void) {
 		// display necessary information (duration, wav header etc)
 		// calculate fast dBs
 		// send fast dBs to web (php program on www.cc.puv.fi)	
-		break; 		// for testing, just run the loop once
+		//break; 		// for testing, just run the loop once
 	}
 }
--- sound.c
+++ sound.c
@@ -1,6 +1,6 @@
 #include "sound.h"
 #include <stdio.h>
-
+#include <math.h>
 // function definition of displayBar()
 // this function opens the "test1.wav" file and read the second part (data) of 
 // the file. The samples should be in S16_LE format, and there are 16000 of
@@ -9,6 +9,8 @@
 void displayBar(char filename[]){
 	FILE *fp;
 	short int samples[SAMPLERATE];
+	double sum_200, rms_80[80], dB;
+	int i, j;
 	WAVHeader myhdr;		// dummy header to skip over the reading from the file
 	fp = fopen(filename, "r");
 	if(fp == NULL){
@@ -17,12 +19,23 @@ void displayBar(char filename[]){
 	}
 	fread(&myhdr, sizeof(WAVHeader), 1, fp);
 	fread(&samples, sizeof(short), SAMPLERATE, fp);
-	fclose(fp);
+	fclose(fp);	
 	// all the samples of lsec are read to the array samples[], we need to run a loop 80 times
 	// for 80 bars on the screen, and each iteration of this loop will calculate a RMS value
 	// of 200 samples
+	clearScreen();
 	for(i=0; i<80; i++){
-		
+		for(j=0,sum_200=0.0; j<200; j++){
+			sum_200 += samples[j+i*200]*samples[j+i*200];
+		}
+		rms_80[i] = sqrt(sum_200/200);
+		dB = 20*log10(rms_80[i]);
+#ifdef DEBUG
+		printf("RMS[%d] = %10.4f = %10.4fdB\n", i,  rms_80[i], dB);
+#else
+		// in order to display sound value in a screen, we need to use decibel
+		bar(i, dB);
+#endif
 	}
 }
 
@@ -36,7 +49,7 @@ void displayWAVheader(char filename[]){
 		return;
 	}
 	fread(&myhdr, sizeof(WAVHeader), 1, fp);
-	fclosep);
+	fclose(fp);
 	printID(myhdr.chunkID);
 	printf("Chunk size: %d\n", myhdr.chunkSize);
 	printID(myhdr.format);
